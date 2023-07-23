dd820aa8be7c8c85a2fb4539adf4b3c1a5f00f6a
Russ Burnham <russ.burnham@yahoo.com>
2023-07-23 00:28:25 +0000

    SocialMediaBlogAPI-RussBurnham

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -6,41 +6,41 @@ import java.util.List;
 
 public class MessageService {
 
-    private MessageDao messageDao;
+	private MessageDao messageDao;
 
-    public MessageService() {
-        messageDao = new MessageDao();
-    }
+	public MessageService() {
+		messageDao = new MessageDao();
+	}
 
-    public MessageService(MessageDao messageDao) {
-        this.messageDao = messageDao;
-    }
+	public MessageService(MessageDao messageDao) {
+		this.messageDao = messageDao;
+	}
 
-    public Message createMessage(Message message) {
-        if (message.getMessage_text().isBlank() || message.getMessage_text().length() > 254) {
-            return null;
-        }
-        return messageDao.createMessage(message);
-    }
+	public Message createMessage(Message message) {
+		if (message.getMessage_text().isBlank() || message.getMessage_text().length() > 254) {
+			return null;
+		}
+		return messageDao.createMessage(message);
+	}
 
-    public boolean deleteMessage(int message_id) {
-        return messageDao.deleteMessage(message_id);
-    }
+	public boolean deleteMessage(int message_id) {
+		return messageDao.deleteMessage(message_id);
+	}
 
-    public Message getMessageById(int message_id) {
-        return messageDao.getMessageById(message_id);
-    }
+	public Message getMessageById(int message_id) {
+		return messageDao.getMessageById(message_id);
+	}
 
-    public List<Message> getMessageByAccountId(int posted_by) {
-        return messageDao.getMessageByAccountId(posted_by);
-    }
+	public List<Message> getMessageByAccountId(int posted_by) {
+		return messageDao.getMessageByAccountId(posted_by);
+	}
 
-    public List<Message> getAllMessages() {
-        return messageDao.getAllMessages();
-    }
+	public List<Message> getAllMessages() {
+		return messageDao.getAllMessages();
+	}
 
-    public Message updateMessageById(Message message) {
-        return messageDao.updateMessageById(message);
-    }
+	public Message updateMessageById(Message message) {
+		return messageDao.updateMessageById(message);
+	}
 
 }
